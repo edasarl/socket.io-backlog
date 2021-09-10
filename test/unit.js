f743946dd8548867a9b68a1f69cf3cbf142ee982
Jérémy Lal <kapouer@melix.org>
2021-09-10 14:58:28 +0200

    Tests

--- test/unit.js
+++ test/unit.js
@@ -3,7 +3,7 @@ const ClientIO = require('socket.io-client');
 const backlog = require('../');
 
 describe("backlog", function () {
-	this.timeout(5000);
+	this.timeout(3000);
 	const httpServer = require("http").createServer();
 	let iourl = "http://";
 	let master;
@@ -11,7 +11,7 @@ describe("backlog", function () {
 	const io = require('socket.io')(httpServer);
 	io.adapter(backlog({
 		logSize: 100,
-		cacheSize: 2
+		cacheSize: 10
 	}));
 	io.on("connection", function(socket) {
 		socket.on('join', function (data) {
@@ -38,18 +38,98 @@ describe("backlog", function () {
 		});
 	});
 	after(function () {
+		master.close();
 		httpServer.close();
 	});
+	beforeEach(function () {
+		this.client = ClientIO(iourl);
+	});
+	afterEach(function () {
+		this.client.close();
+	});
 
-	it("should get last message", function (done) {
-		master.emit({ room: 'test1', test: 1 });
-		const client = ClientIO(iourl);
+
+
+	it("should broadcast a message", function (done) {
+		const client = this.client;
 		client.on('connect', function () {
-			client.emit('join', {room: 'test1', mtime: 0});
+			client.emit('join', { room: 'test1', mtime: 0 });
+			master.emit('message', { room: 'test1', test: 1 });
 		});
 		client.on('message', function (msg) {
-			console.log(msg);
+			assert.strictEqual(msg.test, 1);
 			done();
+		});
+	});
+
+	it("should catch up a message", function (done) {
+		master.emit('message', { room: 'test2', test: 2 });
+		const client = this.client;
+		client.on('connect', function () {
+			client.emit('join', { room: 'test2', mtime: 0 });
+		});
+		client.on('message', function (msg) {
+			assert.strictEqual(msg.test, 2);
+			done();
+		});
+	});
+
+	it("should catch up only last message", function (done) {
+		const client = this.client;
+		const room = 'test3';
+		const test = 30;
+		client.on('connect', function () {
+			client.emit('join', { room });
+			master.emit('message', { room, test });
+		});
+		let total = 0;
+		client.on('message', function (msg) {
+			total++;
+			if (msg.test === test) {
+				const witness = ClientIO(iourl);
+				witness.on('connect', function () {
+					witness.emit('join', { room, mtime: Date.now() });
+					master.emit('message', { room, test: test + 1 });
+				});
+				witness.on('message', function (msg) {
+					assert.strictEqual(msg.test, test + 1);
+					assert.strictEqual(total, 2);
+					witness.close();
+					done();
+				});
+			}
+		});
+	});
+
+	it("should catch up last two messages", function (done) {
+		const client = this.client;
+		const room = 'test4';
+		const test = 40;
+		const refTime = Date.now();
+		client.on('connect', function () {
+			client.emit('join', { room });
+			master.emit('message', { room, test });
+		});
+		let total = 0;
+		client.on('message', function (msg) {
+			total++;
+			if (msg.test !== test) return;
+			const witness = ClientIO(iourl);
+			witness.on('connect', function () {
+				witness.emit('join', { room, mtime: refTime });
+			});
+			let count = 0;
+			witness.on('message', function (msg) {
+				if (count++ == 0) {
+					master.emit('message', { room, test: test + 1 });
+					assert.strictEqual(msg.test, test);
+				} else {
+					assert.strictEqual(msg.test, test + 1);
+					assert.strictEqual(total, 2);
+					witness.close();
+					done();
+				}
+			});
 		});
 	});
 });
